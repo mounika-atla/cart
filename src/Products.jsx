241393dc4149097f6d6db621f9bfb9bb6c2c15fe
mounika-atla <atluruvenkatamounika61912@gmail.com>
2024-02-06 16:18:30 +0530

    shopping-cart1

--- src/Products.jsx
+++ src/Products.jsx
@@ -5,6 +5,37 @@ import { Outlet } from "react-router-dom";
 function Products(props){
     return(
         <>
+        <nav className="navbar navbar-expand-lg bg-body-tertiary w-100 ">
+  <div class="container-fluid bg-danger-subtle">
+    <a class="navbar-brand" href="#"><b>Home</b></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="#"><b>Daily Deals</b></a>
+        </li>
+       
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+           <b> Categories</b>
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="#">Mens Wear</a></li>
+            <li><a class="dropdown-item" href="#">Womens Wear</a></li>
+            <li><hr class="dropdown-divider"/></li>
+            <li><a class="dropdown-item" href="#">Accesories</a></li>
+          </ul>
+        </li>
+      </ul>
+      <form class="d-flex" role="search">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-info" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
         <div  className=" border border-1  w-100">
         <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
           <div class="carousel-indicators">
